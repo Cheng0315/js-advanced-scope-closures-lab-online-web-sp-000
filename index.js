6c8d7f54b56155a81dd9e24d0918e62c4f6de7cc
Cheng Vue <csalvue@gmail.com>
2019-02-26 21:13:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function produceDrivingRange(blockRange) {
-  return function(arg) {
-    return arg - blockRange
+  return function(arg1, arg2) {
+    let range = Math.abs(parseInt(arg1.slice(0, -2) - parseInt(arg2.slice(0, -2))))
+    if (range <= 10) {
+      return `within range by ${range}`
+    } else {
+      return `${range} blocks out of range`
+    }
   }
 }
